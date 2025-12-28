de45be28562519a670ad7b0a6d4bd773f0dc5d80
Lelona Ntshiba <lelonantshiba@gmail.com>
2025-12-28 11:59:41 +0200

    Update CORS origins and configure dynamic port

--- backend/main.py
+++ backend/main.py
@@ -15,6 +15,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://localhost:3000",
+    "https://lolothemisfit.github.io/portfolio/#about"
 ]
 
 app.add_middleware(
@@ -100,4 +101,5 @@ def post_contact(contact: Contact, background_tasks: BackgroundTasks):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
